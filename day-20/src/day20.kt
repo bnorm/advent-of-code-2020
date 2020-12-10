eb19edc239f642c89b1c29c1f702fb15ac6aa661
Brian Norman <brian.norman@datasite.com>
2020-12-10 11:52:50 -0600

    Create templates for remaining days

---
+++ day-20/src/day20.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val sample = """
+    """.trimIndent()
+
+    val input = sample.trim().splitToSequence("\n")
+//    val input = readResourceText("input.txt").trim().splitToSequence("\n")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.name = "advent-of-code-2020"
 
 include(":util")
-for (i in 1..10) {
+for (i in 1..25) {
     include(":day-${i.toString().padStart(2, '0')}")
 }
