bb226a5c322c88df84344dffeb9c465171311e20
Brian Norman <brian.norman@datasite.com>
2020-12-15 09:15:22 -0600

    Day 14 and 15 solution

--- day-14/src/day14.kt
+++ day-14/src/day14.kt
@@ -1,7 +1,80 @@
 fun main() {
     val sample = """
+        mask = 000000000000000000000000000000X1001X
+        mem[42] = 100
+        mask = 00000000000000000000000000000000X0XX
+        mem[26] = 1
     """.trimIndent()
 
-    val input = sample.trim().splitToSequence("\n")
-//    val input = readResourceText("input.txt").trim().splitToSequence("\n")
+//    val input = sample.trim().splitToSequence("\n")
+    val input = readResourceText("input.txt").trim().splitToSequence("\n")
+
+    val operations = input.map {
+        val (op, value) = it.split(" = ")
+        op to value
+    }
+
+    run {
+        var mask = "X".repeat(36)
+        val memory = mutableMapOf<Int, String>()
+
+        fun setMemory(address: Int, value: String) {
+            memory[address] = value.zip(mask) { v, m -> if (m == 'X') v else m }.joinToString("")
+        }
+
+        operations.forEach { (op, value) ->
+            if ("mask" == op) {
+                mask = value
+            } else {
+                val (address) = op.extract("mem\\[(\\d+)]")!!
+                setMemory(address.toInt(), value.toLong().toString(2).padStart(36, '0'))
+            }
+//            println("mask = $mask")
+//            println("memory = $memory")
+        }
+
+        val part1 = memory.values.map { it.toLong(2) }.sum()
+
+        println("part1=$part1")
+    }
+
+    run {
+        var mask = "X".repeat(36)
+        val memory = mutableMapOf<String, String>()
+
+        fun addressPermutations(address: String): List<String> {
+            return when (address.indexOf('X')) {
+                -1 -> listOf(address)
+                else -> addressPermutations(address.replaceFirst('X', '0')) +
+                        addressPermutations(address.replaceFirst('X', '1'))
+            }
+        }
+
+        fun setMemory(address: String, value: String) {
+//            println("mem[$address] = $value")
+            val masked = address.zip(mask) { v, m -> if (m == '0') v else m }.joinToString("")
+
+            for (a in addressPermutations(masked)) {
+                memory[a] = value
+            }
+        }
+
+        operations.forEach { (op, value) ->
+            if ("mask" == op) {
+                mask = value
+            } else {
+                val (address) = op.extract("mem\\[(\\d+)]")!!
+                setMemory(
+                    address.toLong().toString(2).padStart(36, '0'),
+                    value.toLong().toString(2).padStart(36, '0')
+                )
+            }
+//            println("mask = $mask")
+//            println("memory = $memory")
+        }
+
+        val part2 = memory.values.map { it.toLong(2) }.sum()
+
+        println("part2=$part2")
+    }
 }
--- day-15/src/day15.kt
+++ day-15/src/day15.kt
@@ -1,7 +1,45 @@
 fun main() {
     val sample = """
+        0,3,6
     """.trimIndent()
 
-    val input = sample.trim().splitToSequence("\n")
-//    val input = readResourceText("input.txt").trim().splitToSequence("\n")
+//    val input = sample.trim().splitToSequence(",")
+    val input = readResourceText("input.txt").trim().splitToSequence(",")
+
+    val startingNumbers = input.map { it.toLong() }
+
+    val sequence = sequence {
+        val memory0 = mutableMapOf<Long, Int>()
+        val memory1 = mutableMapOf<Long, Int>()
+        var i = 0
+        fun commitToMemory(n: Long) {
+            val prev = memory0[n]
+            memory0[n] = i++
+            if (prev != null) memory1[n] = prev
+        }
+
+        for (n in startingNumbers) {
+            yield(n)
+            commitToMemory(n)
+        }
+
+        var prev = startingNumbers.last()
+        while (true) {
+            // latest index is always (i - 1) because it was the previous value
+            val n = ((i - 1) - (memory1[prev] ?: (i - 1))).toLong()
+            yield(n)
+            commitToMemory(n)
+            prev = n
+        }
+    }
+
+    val part1 = sequence
+        .take(2020)
+        .last()
+    println("part1=$part1")
+
+    val part2 = sequence
+        .take(30_000_000)
+        .last()
+    println("part1=$part2")
 }
