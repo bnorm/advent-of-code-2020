c462eedc87404606306dcf8f8979fa606b064a5a
Brian Norman <brian.norman@datasite.com>
2020-12-18 00:28:14 -0600

    Day 17 and 18 solutions

--- day-17/src/day17.kt
+++ day-17/src/day17.kt
@@ -1,7 +1,140 @@
+import kotlin.math.abs
+
+data class Point(val x: Int, val y: Int, val z: Int, val w: Int) {
+    companion object {
+        val ZERO = Point(0, 0, 0, 0)
+    }
+
+    operator fun plus(other: Point): Point {
+        return Point(x + other.x, y + other.y, z + other.z, w + other.w)
+    }
+}
+
 fun main() {
     val sample = """
+        .#.
+        ..#
+        ###
     """.trimIndent()
 
-    val input = sample.trim().splitToSequence("\n")
-//    val input = readResourceText("input.txt").trim().splitToSequence("\n")
+//    val input = sample.trim().splitToSequence("\n")
+    val input = readResourceText("input.txt").trim().splitToSequence("\n")
+
+    val active = input.flatMapIndexed { r, line ->
+        line.mapIndexedNotNull { c, state ->
+            if (state == '#') Point(c, r, 0, 0)
+            else null
+        }
+    }.toSet()
+
+    run {
+        var iter = active.normalize()
+//    println(iter.toDisplay())
+        repeat(6) {
+            iter = propagate(iter).filter { it.w == 0 }.toSet()
+//        println(iter.toDisplay())
+        }
+        println("part1=${iter.size}")
+    }
+
+    run {
+        var iter = active.normalize()
+//    println(iter.toDisplay())
+        repeat(6) {
+            iter = propagate(iter)
+//        println(iter.toDisplay())
+        }
+        println("part2=${iter.size}")
+    }
+}
+
+@OptIn(ExperimentalStdlibApi::class)
+private fun Set<Point>.normalize(): Set<Point> {
+    val maxX = maxOfOrNull { it.x } ?: 0
+    val maxY = maxOfOrNull { it.y } ?: 0
+    val maxZ = maxOfOrNull { it.z } ?: 0
+    val maxW = maxOfOrNull { it.w } ?: 0
+
+    return map { (x, y, z, w) ->
+        Point(x - maxX / 2, y - maxY / 2, z - maxZ / 2, w - maxW / 2)
+    }.toSet()
+}
+
+@OptIn(ExperimentalStdlibApi::class)
+fun propagate(active: Set<Point>): Set<Point> {
+    val rX = ((active.minOfOrNull { it.x } ?: 0) - 1)..((active.maxOfOrNull { it.x } ?: 0) + 1)
+    val rY = ((active.minOfOrNull { it.y } ?: 0) - 1)..((active.maxOfOrNull { it.y } ?: 0) + 1)
+    val rZ = ((active.minOfOrNull { it.z } ?: 0) - 1)..((active.maxOfOrNull { it.z } ?: 0) + 1)
+    val rW = ((active.minOfOrNull { it.w } ?: 0) - 1)..((active.maxOfOrNull { it.w } ?: 0) + 1)
+
+    fun activeOrthogonal(point: Point): Set<Point> = buildSet {
+        inSpace(-1..1, -1..1, -1..1, -1..1) { vector ->
+            if (vector != Point.ZERO) {
+                val element = point + vector
+                if (element in active) {
+                    add(element)
+                }
+            }
+        }
+    }
+
+    return buildSet {
+        inSpace(rX, rY, rZ, rW) { point ->
+            when (point) {
+                in active -> {
+                    if (activeOrthogonal(point).size in 2..3) {
+                        add(point) // remains active
+                    } // else becomes inactive
+                }
+                else -> {
+                    if (activeOrthogonal(point).size == 3) {
+                        add(point) // becomes active
+                    } // else remains inactive
+                }
+            }
+        }
+    }
+}
+
+fun Set<Point>.toDisplay(): String = buildString {
+    val active = this@toDisplay
+
+    val max = active.map { maxOf(abs(it.x), abs(it.y)) }.maxOrNull() ?: 1
+    val minZ = active.minOfOrNull { it.z } ?: 0
+    val maxZ = active.maxOfOrNull { it.z } ?: 0
+    val minW = active.minOfOrNull { it.w } ?: 0
+    val maxW = active.maxOfOrNull { it.w } ?: 0
+
+    var prevZ = minZ - 1
+    var prevW = minW - 1
+    var prevY = max + 1
+    inSpace(-max..max, -max..max, minZ..maxZ, minW..maxW) { point ->
+        if (prevY < point.y) {
+            appendLine()
+        }
+        prevY = point.y
+        if (point.z != prevZ || point.w != prevW) {
+            appendLine()
+            appendLine("z=${point.z}, w=${point.w}")
+            prevZ = point.z
+            prevW = point.w
+        }
+        append(if (point in active) '#' else '.')
+    }
+}
+
+fun inSpace(r: IntRange, block: (Point) -> Unit) {
+    inSpace(r, r, r, r, block)
+}
+
+fun inSpace(rX: IntRange, rY: IntRange, rZ: IntRange, rW: IntRange, block: (Point) -> Unit) {
+    for (w in rW) {
+        for (z in rZ) {
+            for (y in rY) {
+                for (x in rX) {
+                    block(Point(x, y, z, w))
+                }
+            }
+        }
+    }
 }
--- day-18/src/day18.kt
+++ day-18/src/day18.kt
@@ -1,7 +1,98 @@
 fun main() {
     val sample = """
+        1 + (2 * 3) + (4 * (5 + 6))
+        2 * 3 + (4 * 5)
+        5 + (8 * 3 + 9 + 3 * 4 * 3)
+        5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))
+        ((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2
     """.trimIndent()
 
-    val input = sample.trim().splitToSequence("\n")
-//    val input = readResourceText("input.txt").trim().splitToSequence("\n")
+//    val input = sample.trim().splitToSequence("\n")
+    val input = readResourceText("input.txt").trim().splitToSequence("\n")
+
+    val part1 = input.map {
+        calc(tokenize(it))
+    }.sum()
+    println("part1=$part1")
+
+    val part2 = input.map {
+        calcAdv(tokenize(it))
+    }.sum()
+    println("part2=$part2")
+}
+
+fun tokenize(line: String) = iterator<String> {
+    var i = 0
+    while (i < line.length) {
+        when (val c = line[i]) {
+            ' ' -> {
+                i++
+            }
+            '(', ')', '*', '+' -> {
+                yield(c.toString())
+                i++
+            }
+            in '0'..'9' -> {
+                val start = i
+                while (i < line.length && line[i] in '0'..'9') i++
+                yield(line.substring(start, i))
+            }
+        }
+    }
+}
+
+fun calc(tokens: Iterator<String>): Long {
+    var value = 0L
+    var op = "+"
+
+    fun op(next: Long) {
+        when (op) {
+            "+" -> value += next
+            "*" -> value *= next
+            else -> TODO("op=\"$op\"")
+        }
+    }
+
+    while (tokens.hasNext()) {
+        when (val token = tokens.next()) {
+            ")" -> return value
+            "(" -> op(calc(tokens))
+            "*", "+" -> op = token
+            else -> op(token.toLongOrNull() ?: TODO("token=\"$token\""))
+        }
+    }
+    return value
+}
+
+fun calcAdv(tokens: Iterator<String>): Long {
+    val values = ArrayDeque<Long>()
+    var value = 0L
+    var op = "+"
+
+    fun op(next: Long) {
+        when (op) {
+            "+" -> value += next
+            "*" -> {
+                values.add(value)
+                value = next
+            }
+            else -> TODO("op=\"$op\"")
+        }
+    }
+
+    fun finalize(): Long {
+        if (value != 0L) values.add(value)
+        return values.reduce { acc, n -> acc * n }
+    }
+
+    while (tokens.hasNext()) {
+        when (val token = tokens.next()) {
+            ")" -> return finalize()
+            "(" -> op(calcAdv(tokens))
+            "*", "+" -> op = token
+            else -> op(token.toLongOrNull() ?: TODO("token=\"$token\""))
+        }
+    }
+
+    return finalize()
 }
